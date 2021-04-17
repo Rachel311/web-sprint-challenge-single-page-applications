2d1735c59b5fbc4fcf0123161cc36fab9d8c10d7
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-04-16 22:09:17 -0600

    Completed ReadMe and about half of App and Form

--- README.md
+++ README.md
@@ -31,8 +31,11 @@ Commit your code regularly and meaningfully. This helps both you (in case you ev
 Demonstrate your understanding of this week's concepts by answering the following free-form questions. Edit this document to include your answers after each question.
 
 1. In 1-2 sentences, explain what React's `useRouteMatch` hook is used for.
+It helps you match the components or parents if it's given no arguments.
 1. How would you explain form validation to someone who has never programmed before?
+It's a process that checks if the information given to the user is accurate. 
 1. In 1-2 sentences, define end to end testing.
+It's a testing method that tests a program from end to end. 
 
 ## Instructions
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from "react";
+import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Form from './Components/Form'
 
-const App = () => {
+
+function App() {
+  const [formValue, setFormValue] = useState({
+    pizza_name: "",
+    special_instrutions: "",
+  });
+  const [teamMembers, setTeamMembers] = useState([]);
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setFormValue({...formValue, [name]: value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const newMember = {
+      first_name: formValue.pizza_name.trim(),
+      last_name: formValue.special_instrutions.trim(),
+     };
+    setTeamMembers([...teamMembers, newMember]);
+    setFormValue({pizza_name: "",
+    special_instrutions: "",
+    })
+  };
+
+
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+    <h1>Lambda Eats!</h1>
+      <Form values={formValue} onChange={onChange} onSubmit={onSubmit} />
+      {teamMembers.map((pizza) => {
+        return (
+          <div className="new-submissions" key={pizza.id}>
+            <h2>{pizza.pizza_name}</h2>
+            <h2>{pizza.special_instrutions}</h2>
+            
+          </div>
+        );
+      })}
+    </div>
   );
-};
+}
+
 export default App;
+
